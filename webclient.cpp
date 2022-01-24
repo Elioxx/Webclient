3d4954c03225925436fd79626ae00d3f4b614a62
Elioxx <78585852+Elioxx@users.noreply.github.com>
2022-01-24 19:47:35 +0100

    Completed Webclient

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,51 @@
+#include <thread>
+
 #include "webclient.h"
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    connect(hostnameEntry, &QLineEdit::returnPressed, this, &Webclient::go_button_clicked);
+
+    m_socket = new QTcpSocket(this);
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::readyRead);
+
+}
+
+void Webclient::printdisplayResult(QString webpage)
+{
+    displayResult->clear();
+    displayResult->setText(webpage);
+}
+
+void Webclient::go_button_clicked()
+{
+    if (!hostnameEntry->text().isEmpty())
+    {
+        m_hostname = hostnameEntry->text();
+        printdisplayResult("Input:" + m_hostname);
+        m_socket->connectToHost(m_hostname, m_port);
+        if (!m_socket->waitForConnected(5000))
+        {
+            printdisplayResult("Connection failed!\n");
+            printdisplayResult("Can't open the page " + m_hostname + " beacuse the server could not be found.");
+            m_socket->reset();
+        }
+    }
+    else
+        printdisplayResult("Please enter another adress.");
+}
+
+void Webclient::connected()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::readyRead()
+{
+    printdisplayResult(m_socket->readAll());
+    m_socket->disconnectFromHost();
 }
--- webclient.h
+++ webclient.h
@@ -3,12 +3,29 @@
 
 #include "ui_webclient.h"
 
+#include <QDebug>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+
 class Webclient : public QWidget, private Ui::Webclient
 {
     Q_OBJECT
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+    void go_button_clicked();
+
+private:
+
+    void connected();
+    void readyRead();
+    void printdisplayResult(QString webpage);
+
+    QTcpSocket * m_socket = nullptr;
+    QString m_hostname;
+    const unsigned char m_port = 80;
 };
 
 #endif // WEBCLIENT_H
